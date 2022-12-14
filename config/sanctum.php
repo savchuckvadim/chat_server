25962d3f00dd37bf26b276b0032fbdbcb2f018b4
savchuckvadim <savchuckvadim@gmail.com>
2022-12-14 15:04:28 +0300

    fixed env

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        '45.67.57.72:80, http://45.67.57.72:80, 45.67.57.72:3000, 45.67.57.72, http://45.67.57.72:3000, http://45.67.57.72, http://45.67.57.72:9000, 45.67.57.72:9000, http://localhost:3000, http://localhost:8000, localhost:3000, localhost:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
