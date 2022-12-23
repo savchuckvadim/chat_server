64f8de8957f111aa8844712c541594eeceec9ba4
savchuckvadim <savchuckvadim@gmail.com>
2022-12-23 14:49:06 +0300

    fixed broadcat config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,8 +39,12 @@ return [
                 'host' => env('PUSHER_HOST', 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'http'),
-                'encrypted' => false,
-                'useTLS' => false
+                'encrypted' => true,
+                'useTLS' => false,
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
                 // env('PUSHER_SCHEME', 'https') === 'https',
             ],
             // 'client_options' => [
