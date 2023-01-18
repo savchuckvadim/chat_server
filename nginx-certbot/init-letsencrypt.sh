5fcf4a07113b5db2290f9127cd07c5e8ca0041c9
savchuckvadim <savchuckvadim@gmail.com>
2023-01-18 17:45:27 +0300

    add domain

--- nginx-certbot/init-letsencrypt.sh
+++ nginx-certbot/init-letsencrypt.sh
@@ -5,10 +5,9 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=("nmbrs-chat.store")
+domains=("nmbrs-chat.store, back.nmbrs-chat.store")
 rsa_key_size=4096
-# data_path="./data/certbot"
-data_path="~/chat_server/certbot"
+data_path="/data/certbot"
 
 email="reactlaravelchat@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
