b94071127fb05ca22efea78bac6bdf62c963fe13
savchuckvadim <savchuckvadim@gmail.com>
2023-01-18 17:27:37 +0300

    fixed init-letsencrypt instruction domains

--- nginx-certbot/init-letsencrypt.sh
+++ nginx-certbot/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=("45.147.179.218" "nmbrs-chat.store")
+domains=("nmbrs-chat.store")
 rsa_key_size=4096
 # data_path="./data/certbot"
 data_path="~/chat_server/certbot"
